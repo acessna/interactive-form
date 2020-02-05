2bcaa7324dd8716e7b3ff76a0127e03029b36ef4
Adam Cessna <racessna@gmail.com>
2020-02-05 07:16:25 -0600

    working on job field

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 let nameInput = document.getElementById('name');
+let otherJobRoleInput = document.getElementById('other-title');
+const jobRoleOptions = document.getElementById('title');
+
 nameInput.focus();
+
+//otherJobRoleInput.setAttribute('hidden', true);
+
+for (let i = 0; i < jobRoleOptions.length; i++) {
+    jobRoleOptions.addEventListener('change', (e) =>{
+    console.log(e.target[i]);
+
+});
+}
+
+
