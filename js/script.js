d7ec73401e4605203c5926c31e2c468721df7234
Adam Cessna <racessna@gmail.com>
2020-02-10 19:13:43 -0600

    work on showing color options

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 let nameInput = document.getElementById('name');
 let otherJobRoleInput = document.getElementById('other-title');
 const jobRoleOptions = document.getElementById('title');
-const otherOption = document.querySelector('option[value="other"]');
+const colorOptions = document.getElementById('color');
+const designOptions = document.getElementById('design');
+const selectDesignFirstOption = document.createElement('option');
+
+
 
 nameInput.focus();
+
+/* The showOtherJobInput method displays the otherJobRoleInput when
+the 'other' option is selected in the Job Role selector */
 
 const showOtherJobInput = () => {
     otherJobRoleInput.style.display = 'none';
@@ -18,3 +25,59 @@ const showOtherJobInput = () => {
 }
 
 showOtherJobInput();
+
+/* The hideColors function hides the shirt color options and tells the user to first select a shirt them */
+
+const hideColors = () => {
+    selectDesignFirstOption.innerHTML = 'Please Select a Theme';
+    for (let i = 0; i < colorOptions.length; i++) {
+        colorOptions[i].style.display = 'none';
+    }
+    colorOptions.appendChild(selectDesignFirstOption);
+    selectDesignFirstOption.style.display = 'block';
+    colorOptions.selectedIndex = '6';
+}
+
+hideColors();
+
+const showJsPuns = () => {
+    for (let i = 0; i < 3; i++) {
+        colorOptions[i].style.display = 'block';
+    }
+    
+    for (let i = 0; i > 3; i++) {
+        colorOptions[i].style.display = 'none';
+    }
+    colorOptions.selectedIndex = '0';
+    selectDesignFirstOption.style.display = 'none'
+}
+
+const showHeartJs = () => {
+       
+    for (let i = 0; i < 3; i++) {
+        colorOptions[i].style.display = 'none';
+    }
+    for (let i = 0; i > 3; i++) {
+        colorOptions[i].style.display = 'block';
+    }
+    
+
+    selectDesignFirstOption.style.display = 'none'
+}
+
+const showThemeOptions = () => {
+    let selectThemeOption = designOptions[0];
+    selectThemeOption.value = 'select theme';
+
+    designOptions.addEventListener('input', (e) => {
+        if(e.target.value === 'js puns'){
+            showJsPuns();
+        } else if(e.target.value === 'heart js'){
+            showHeartJs();
+        } else if(e.target.value === 'select theme'){
+            hideColors();
+        }
+    });
+
+}
+showThemeOptions();
